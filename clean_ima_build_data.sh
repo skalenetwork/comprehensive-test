d581b6321cf9f1e64b031dd516716938a374fda9
Sergiy Lavrynenko <sergiy@skalelabs.com>
2023-10-12 16:25:33 +0100

    New separated IMA mode support

--- clean_ima_build_data.sh
+++ clean_ima_build_data.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-cd IMA/proxy; npx hardhat clean; cd ../..
-rm -rf ./IMA/proxy/artifacts
-rm -rf ./IMA/proxy/.openzeppelin/unknown*.json
-rm -rr ./IMA/proxy/data/*.json
-rm -rf ./IMA/proxy/cache
-rm -rf ./IMA/proxy/predeployed/dist
-rm -rf ./IMA/proxy/predeployed/venv
-rm -rf ./IMA/proxy/predeployed/version.txt
-rm -rf ./IMA/proxy/predeployed/dist
-rm -rf ./IMA/proxy/predeployed/src/ima_predeployed/artifacts/*
-touch  ./IMA/proxy/predeployed/src/ima_predeployed/artifacts/.gitkeep
-rm -rf ./IMA/proxy/predeployed/src/*info
-rm -rf ./IMA/proxy/predeployed/test/additional.json
-rm -rf ./IMA/proxy/typechain
-rm -rf ./IMA/test-tokens/artifcats
-rm -rf ./IMA/test-tokens/cache
+export IMA_CONTRACTS_REPO_ROOT_DIR=./ima-agent/IMA
+export RESTORE_CURRENT_DIR=$(pwd)
+
+cd $IMA_CONTRACTS_REPO_ROOT_DIR/proxy; npx hardhat clean; cd $RESTORE_CURRENT_DIR
+rm -rf $IMA_CONTRACTS_REPO_ROOT_DIR/proxy/artifacts
+rm -rf $IMA_CONTRACTS_REPO_ROOT_DIR/proxy/.openzeppelin/unknown*.json
+rm -rr $IMA_CONTRACTS_REPO_ROOT_DIR/proxy/data/*.json
+rm -rf $IMA_CONTRACTS_REPO_ROOT_DIR/proxy/cache
+rm -rf $IMA_CONTRACTS_REPO_ROOT_DIR/proxy/predeployed/dist
+rm -rf $IMA_CONTRACTS_REPO_ROOT_DIR/proxy/predeployed/venv
+rm -rf $IMA_CONTRACTS_REPO_ROOT_DIR/proxy/predeployed/version.txt
+rm -rf $IMA_CONTRACTS_REPO_ROOT_DIR/proxy/predeployed/dist
+rm -rf $IMA_CONTRACTS_REPO_ROOT_DIR/proxy/predeployed/src/ima_predeployed/artifacts/*
+touch  $IMA_CONTRACTS_REPO_ROOT_DIR/proxy/predeployed/src/ima_predeployed/artifacts/.gitkeep
+rm -rf $IMA_CONTRACTS_REPO_ROOT_DIR/proxy/predeployed/src/*info
+rm -rf $IMA_CONTRACTS_REPO_ROOT_DIR/proxy/predeployed/test/additional.json
+rm -rf $IMA_CONTRACTS_REPO_ROOT_DIR/proxy/typechain
+rm -rf $IMA_CONTRACTS_REPO_ROOT_DIR/test-tokens/artifcats
+rm -rf $IMA_CONTRACTS_REPO_ROOT_DIR/test-tokens/cache
 
